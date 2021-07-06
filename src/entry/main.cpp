6bc2808fe74ed190e838447a6829e22fe45dde54
杨晓宇 <1329218916@qq.com>
2021-07-06 16:12:23 +0800

    feat: 🎸 GEM Layout added

--- src/entry/main.cpp
+++ src/entry/main.cpp
@@ -1,6 +1,7 @@
 
 #include <ogdf/basic/GraphAttributes.h>
 #include <ogdf/graphalg/steiner_tree/EdgeWeightedGraph.h>
+#include <ogdf/energybased/GEMLayout.h>
 #include <ogdf/energybased/FMMMLayout.h>
 #include <ogdf/energybased/PivotMDS.h>
 #include <ogdf/energybased/StressMinimization.h>
@@ -199,6 +200,53 @@ EM_PORT_API(float*) SM(int node_num, int link_num, int* source, int* target, dou
 	model->setEdgeCosts(edgeCosts);
 	model->setIterations(numberOfIterations);
 	model->useEdgeCostsAttribute(useEdgeCostsAttribute);
+
+    model->call(GA);
+
+	float* re = (float*)malloc(node_num * 2 * 4);
+    for(int i = 0; i < node_num; ++i) {
+        re[i * 2] = GA.x(nodes[i]);
+        re[i * 2 + 1] = GA.y(nodes[i]);
+    }
+
+    return re;
+}
+
+EM_PORT_API(float*) GEM(int node_num, int link_num, int* source, int* target, double* edgesWeight, double* nodesX, double* nodesY, int attractionFormula, double desiredLength, double gravitationalConstant, double initialTemperature, double maximalDisturbance, double minDistCC, double minimalTemperature, int numberOfRounds, double oscillationAngle, double oscillationSensitivity, double pageRatio, double rotationAngle, double rotationSensitivity){
+	node* nodes;
+	Graph G;
+	GraphAttributes GA(G, GraphAttributes::nodeGraphics | GraphAttributes::edgeGraphics | GraphAttributes::edgeDoubleWeight);
+
+	nodes = new node[node_num];
+	for (int i = 0; i < node_num; i++){
+		nodes[i] = G.newNode();
+		GA.x(nodes[i]) = nodesX[i];
+		GA.y(nodes[i]) = nodesY[i];
+	}
+
+	edge e;
+
+	for (int i = 0; i < link_num; i++) {
+		e = G.newEdge(nodes[source[i]], nodes[target[i]]);
+		GA.bends(e);
+		GA.doubleWeight(e) = edgesWeight[i];
+	}
+	//LayoutModule
+	GEMLayout *model = new GEMLayout();
+
+	model->attractionFormula(attractionFormula);
+	model->desiredLength(desiredLength);
+	model->gravitationalConstant(gravitationalConstant);
+	model->initialTemperature(initialTemperature);
+	model->maximalDisturbance(maximalDisturbance);
+	model->minDistCC(minDistCC);
+	model->minimalTemperature(minimalTemperature);
+	model->numberOfRounds(numberOfRounds);
+	model->oscillationAngle(oscillationAngle);
+	model->oscillationSensitivity(oscillationSensitivity);
+	model->pageRatio(pageRatio);
+	model->rotationAngle(rotationAngle);
+	model->rotationSensitivity(rotationSensitivity);
 
     model->call(GA);
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import gem from "./layouts/gem.layout"
 import fm3 from "./layouts/fm3"
 import pmds from "./layouts/pmds.layout"
 import sm from "./layouts/sm.layout"
 
-export { fm3, pmds, sm }
+export { fm3, gem, pmds, sm }
