ed20b4efb5e49edff00b7c60e2a24f6a9a33a6a2
JackieAnxis <jackieanxis@gmail.com>
2021-08-17 17:06:34 +0800

    feat: 🎸 change layout from function to class

--- packages/ogdf.js/src/layouts/creater.js
+++ packages/ogdf.js/src/layouts/creater.js
@@ -1,6 +1,11 @@
 import initOGDF from '../entry/rawogdf'
 import { createWorker } from '../utils/worker-helper'
-import { getDefaultValueOfParameters, getOriginParameterSequence, getOriginalParameters, PARAMETER_TYPE } from '../utils/parameters'
+import {
+    getDefaultValueOfParameters,
+    getOriginParameterSequence,
+    getOriginalParameters,
+    PARAMETER_TYPE
+} from '../utils/parameters'
 
 export default function createLayout(NAME, OUR_PARAMETERS, ORIGIN_PARAMETERS, ATTRIBUTES) {
     const PARAMETERS = {
@@ -10,178 +15,226 @@ export default function createLayout(NAME, OUR_PARAMETERS, ORIGIN_PARAMETERS, AT
 
     const ORIGIN_PARAMETER_SEQUENCE = getOriginParameterSequence(ORIGIN_PARAMETERS)
 
-    function layout(graph, params, callback) {
-        // overwrite default parameters by user defined parameters
-        let parameters = {
-            ...getDefaultValueOfParameters(PARAMETERS),
-            ...params
-        }
+    class Layout {
+        constructor({ graph, parameters, callback }) {
+            this._sourceIndexArray = []
+            this._targetIndexArray = []
+            this._orderedAttributes = []
 
-        // ogdf-defined parameters should keep their orders
-        const originalParameters = getOriginalParameters(parameters, ORIGIN_PARAMETERS, ORIGIN_PARAMETER_SEQUENCE)
+            this.name = NAME
+            this._graph = { nodes: [], links: [] }
+            this.graph(graph)
 
-        const graphCopy = JSON.parse(JSON.stringify(graph)) // ! maybe we dont need to copy it
-        const N = graphCopy.nodes.length
-        const M = graphCopy.links.length
-
-        const id2index = {}
-        for (let i = 0; i < N; ++i) {
-            if (graphCopy.nodes[i]['id'] in id2index) {
-                throw Error('Duplicated Node ID') // duplicated node id is not allowed
-            } else id2index[graphCopy.nodes[i]['id']] = i
-        }
-
-        const sourceIndexArray = []
-        const targetIndexArray = []
-        for (let i = 0; i < M; ++i) {
-            sourceIndexArray.push(id2index[graphCopy.links[i].source])
-            targetIndexArray.push(id2index[graphCopy.links[i].target])
-        }
-
-        // sort attributes according to their order (sequence)
-        const nodeAttributes = ATTRIBUTES.node
-        const linkAttributes = ATTRIBUTES.link
-        const unorderedAttributes = {}
-        nodeAttributes?.forEach((attr) => {
-            unorderedAttributes[attr.name] = {
-                type: attr.type,
-                value: graphCopy.nodes.map(attr.mapper)
+            // overwrite default parameters by user defined parameters
+            this._parameters = {
+                ...getDefaultValueOfParameters(PARAMETERS)
             }
-        })
-        linkAttributes?.forEach((attr) => {
-            unorderedAttributes[attr.name] = {
-                type: attr.type,
-                value: graphCopy.links.map(attr.mapper)
-            }
-        })
-        const orderedAttributes = []
-        ATTRIBUTES.sequence?.forEach((name) => {
-            orderedAttributes.push(unorderedAttributes[name])
-        }) // ! Ensure the order of attributes
+            this.parameters(parameters)
 
-        function createOGDFProcess() {
-            function OGDFProcess(
-                {
-                    NAME,
-                    PARAMETER_TYPE,
-                    initOGDF,
-                    N,
-                    M,
-                    sourceIndexArray,
-                    targetIndexArray,
-                    orderedAttributes,
-                    originalParameters
-                },
-                callback
-            ) {
-                initOGDF().then(function (Module) {
-                    let source = Module._malloc(4 * M)
-                    let target = Module._malloc(4 * M)
-                    for (let i = 0; i < M; ++i) {
-                        Module.HEAP32[source / 4 + i] = sourceIndexArray[i]
-                        Module.HEAP32[target / 4 + i] = targetIndexArray[i]
-                    }
-                    const mallocAttributes = orderedAttributes.map((attr) => {
-                        let bytes = 4
-                        if (attr.type == PARAMETER_TYPE.DOUBLE) {
-                            bytes = 8
-                        }
-                        const malloc = Module._malloc(bytes * attr.value.length)
-                        let heap = Module.HEAP32
-                        if (attr.type == PARAMETER_TYPE.DOUBLE) {
-                            heap = Module.HEAPF64
-                        }
-                        for (let i = 0; i < attr.value.length; ++i) {
-                            heap[malloc / bytes + i] = attr.value[i]
-                        }
-                        return malloc
-                    })
-                    const result = Module[`_${NAME}`](
+            this._callback = callback
+            this.callback(callback)
+        }
+        run() {
+            // ogdf-defined parameters should keep their orders
+            const originalParameters = getOriginalParameters(
+                this._parameters,
+                ORIGIN_PARAMETERS,
+                ORIGIN_PARAMETER_SEQUENCE
+            )
+            console.log(originalParameters)
+
+            const N = this._graph.nodes.length
+            const M = this._graph.links.length
+
+            function createOGDFProcess() {
+                function OGDFProcess(
+                    {
+                        NAME,
+                        PARAMETER_TYPE,
+                        initOGDF,
                         N,
                         M,
-                        source,
-                        target,
-                        ...mallocAttributes,
-                        ...originalParameters
-                    ) // ! Ensure the order of attributes/parameters
-                    const nodes = []
+                        sourceIndexArray,
+                        targetIndexArray,
+                        orderedAttributes,
+                        originalParameters
+                    },
+                    callback
+                ) {
+                    initOGDF().then(function (Module) {
+                        let source = Module._malloc(4 * M)
+                        let target = Module._malloc(4 * M)
+                        for (let i = 0; i < M; ++i) {
+                            Module.HEAP32[source / 4 + i] = sourceIndexArray[i]
+                            Module.HEAP32[target / 4 + i] = targetIndexArray[i]
+                        }
+                        const mallocAttributes = orderedAttributes.map((attr) => {
+                            let bytes = 4
+                            if (attr.type == PARAMETER_TYPE.DOUBLE) {
+                                bytes = 8
+                            }
+                            const malloc = Module._malloc(bytes * attr.value.length)
+                            let heap = Module.HEAP32
+                            if (attr.type == PARAMETER_TYPE.DOUBLE) {
+                                heap = Module.HEAPF64
+                            }
+                            for (let i = 0; i < attr.value.length; ++i) {
+                                heap[malloc / bytes + i] = attr.value[i]
+                            }
+                            return malloc
+                        })
+                        const result = Module[`_${NAME}`](
+                            N,
+                            M,
+                            source,
+                            target,
+                            ...mallocAttributes,
+                            ...originalParameters
+                        ) // ! Ensure the order of attributes/parameters
+                        const nodes = []
+                        for (let i = 0; i < N; ++i) {
+                            nodes[i] = {}
+                            nodes[i]['x'] = Module.HEAPF32[(result >> 2) + i * 2]
+                            nodes[i]['y'] = Module.HEAPF32[(result >> 2) + i * 2 + 1]
+                        }
+                        Module._free(source)
+                        Module._free(target)
+                        Module._free_buf(result)
+                        callback(nodes)
+                    })
+                }
+                if (this?.constructor.name == 'DedicatedWorkerGlobalScope') {
+                    console.log('Webworker is working...')
+                    // if it is executed in webworker
+                    addEventListener('message', (e) => {
+                        let message = JSON.parse(e.data)
+                        let initOGDF = message.initOGDF
+                        eval(`initOGDF = ${initOGDF}`)
+                        message.initOGDF = initOGDF
+                        OGDFProcess(message, (nodes) => postMessage(JSON.stringify(nodes)))
+                    })
+                }
+                return OGDFProcess
+            }
+
+            if (this._parameters.useWorker) {
+                const worker = createWorker(createOGDFProcess)
+
+                // post data, including functions/parameters/..., into the webworker
+                worker.postMessage(
+                    JSON.stringify({
+                        NAME,
+                        PARAMETER_TYPE,
+                        initOGDF: initOGDF.toString(), // ! Maybe we can put initOGDF out of web worker
+                        N,
+                        M,
+                        sourceIndexArray: this._sourceIndexArray,
+                        targetIndexArray: this._targetIndexArray,
+                        orderedAttributes: this._orderedAttributes,
+                        originalParameters
+                    })
+                )
+
+                // onmessage listens the returned value from the webworker, namely, the layout
+                worker.onmessage = (e) => {
+                    const nodePositions = JSON.parse(e.data)
                     for (let i = 0; i < N; ++i) {
-                        nodes[i] = {}
-                        nodes[i]['x'] = Module.HEAPF32[(result >> 2) + i * 2]
-                        nodes[i]['y'] = Module.HEAPF32[(result >> 2) + i * 2 + 1]
+                        this._graph.nodes[i].x = nodePositions[i].x
+                        this._graph.nodes[i].y = nodePositions[i].y
                     }
-                    Module._free(source)
-                    Module._free(target)
-                    Module._free_buf(result)
-                    callback(nodes)
-                })
+                    worker.terminate()
+                    this._callback(this._graph)
+                }
+            } else {
+                const OGDFProcess = createOGDFProcess()
+                OGDFProcess(
+                    {
+                        NAME,
+                        PARAMETER_TYPE,
+                        initOGDF,
+                        N,
+                        M,
+                        sourceIndexArray: this._sourceIndexArray,
+                        targetIndexArray: this._targetIndexArray,
+                        orderedAttributes: this._orderedAttributes,
+                        originalParameters
+                    },
+                    (nodes) => {
+                        for (let i = 0; i < N; ++i) {
+                            this._graph.nodes[i]['x'] = nodes[i].x
+                            this._graph.nodes[i]['y'] = nodes[i].y
+                        }
+                        this._callback(this._graph)
+                    }
+                )
             }
-            if (this?.constructor.name == 'DedicatedWorkerGlobalScope') {
-                // if it is executed in webworker
-                addEventListener('message', (e) => {
-                    let message = JSON.parse(e.data)
-                    let initOGDF = message.initOGDF
-                    eval(`initOGDF = ${initOGDF}`)
-                    message.initOGDF = initOGDF
-                    OGDFProcess(message, (nodes) => postMessage(JSON.stringify(nodes)))
-                })
-            }
-            return OGDFProcess
         }
-
-        if (parameters.useWorker) {
-            const worker = createWorker(createOGDFProcess)
-
-            // post data, including functions/parameters/..., into the webworker
-            worker.postMessage(
-                JSON.stringify({
-                    NAME,
-                    PARAMETER_TYPE,
-                    initOGDF: initOGDF.toString(), // ! Maybe we can put initOGDF out of web worker
-                    N,
-                    M,
-                    sourceIndexArray,
-                    targetIndexArray,
-                    orderedAttributes,
-                    originalParameters
-                })
-            )
-
-            // onmessage listens the returned value from the webworker, namely, the layout
-            worker.onmessage = function (e) {
-                const nodePositions = JSON.parse(e.data)
-                for (let i = 0; i < N; ++i) {
-                    graphCopy.nodes[i].x = nodePositions[i].x
-                    graphCopy.nodes[i].y = nodePositions[i].y
-                }
-                worker.terminate()
-                callback(graphCopy)
-            }
-        } else {
-            const OGDFProcess = createOGDFProcess()
-            OGDFProcess(
-                {
-                    NAME,
-                    PARAMETER_TYPE,
-                    initOGDF,
-                    N,
-                    M,
-                    sourceIndexArray,
-                    targetIndexArray,
-                    orderedAttributes,
-                    originalParameters
-                },
-                (nodes) => {
-                    for (let i = 0; i < N; ++i) {
-                        graphCopy.nodes[i]['x'] = nodes[i].x
-                        graphCopy.nodes[i]['y'] = nodes[i].y
+        parameters(parameter, value) {
+            if (parameter) {
+                if (typeof parameter == 'object') {
+                    // this.parameters({ useHighLevelOptions: true })
+                    this._parameters = {
+                        ...this._parameters,
+                        ...parameter
                     }
-                    callback(graphCopy)
+                } else if (typeof parameter == 'string') {
+                    if (value !== undefined) {
+                        // this.parameters("useHighLevelOptions", true )
+                        this._parameters[parameter] = value
+                    } else {
+                        // this.parameters("useHighLevelOptions")
+                        return this._parameters[parameter]
+                    }
                 }
-            )
+            }
+            return JSON.parse(JSON.stringify(this._parameters))
+        }
+        callback(callback) {
+            this._callback = typeof callback == 'function' ? callback : () => {}
+        }
+        graph(graph) {
+            if (graph) {
+                this._graph = graph
+
+                const id2index = {}
+                for (let i = 0; i < this._graph.nodes.length; ++i) {
+                    if (this._graph.nodes[i]['id'] in id2index) {
+                        throw Error('Duplicated Node ID') // duplicated node id is not allowed
+                    } else id2index[this._graph.nodes[i]['id']] = i
+                }
+
+                this._sourceIndexArray = []
+                this._targetIndexArray = []
+                for (let i = 0; i < this._graph.links.length; ++i) {
+                    this._sourceIndexArray.push(id2index[this._graph.links[i].source])
+                    this._targetIndexArray.push(id2index[this._graph.links[i].target])
+                }
+
+                // sort attributes according to their order (sequence)
+                const nodeAttributes = ATTRIBUTES.node
+                const linkAttributes = ATTRIBUTES.link
+                const unorderedAttributes = {}
+                nodeAttributes?.forEach((attr) => {
+                    unorderedAttributes[attr.name] = {
+                        type: attr.type,
+                        value: this._graph.nodes.map(attr.mapper)
+                    }
+                })
+                linkAttributes?.forEach((attr) => {
+                    unorderedAttributes[attr.name] = {
+                        type: attr.type,
+                        value: this._graph.links.map(attr.mapper)
+                    }
+                })
+                this._orderedAttributes = []
+                ATTRIBUTES.sequence?.forEach((name) => {
+                    this._orderedAttributes.push(unorderedAttributes[name])
+                }) // ! Ensure the order of attributes
+            }
+
+            return this._graph
         }
     }
 
-    layout.parameters = PARAMETERS
-    return layout
+    return Layout
 }
