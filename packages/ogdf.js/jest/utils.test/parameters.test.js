73b524815646d809a59313fd3b90a7c67c3820b4
jackie <panjiacheng@zju.edu.cn>
2021-08-24 00:40:49 +0800

    test: 💍 complete fm3 updateparameter test

--- packages/ogdf.js/jest/utils.test/parameters.test.js
+++ packages/ogdf.js/jest/utils.test/parameters.test.js
@@ -2,7 +2,8 @@ const ogdf = require('../../src/index')
 const {
     PARAMETER_TYPE,
     getDefaultParameters,
-    getParameterEntries
+    getParameterEntries,
+    updateParameters
 } = require('../../src/utils/parameters')
 
 describe('Testing getDefaultParameters', () => {
@@ -91,288 +92,336 @@ describe('Testing getDefaultParameters', () => {
 })
 
 describe('Testing updateParameters & getParameterEntries', () => {
-    const fm3 = new ogdf.layouts.energybased.fm3()
+    const fm3Class = ogdf.layouts.energybased.fm3
+    const fm3 = new fm3Class()
     const fm3DefaultParameters = fm3.parameters()
+    const defaultExpected = [
+        /* #region default [Expected default fm3 parameters] */
+        {
+            key: 'useHighLevelOptions',
+            value: false,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.BOOL
+        },
+        {
+            key: 'singleLevel',
+            value: false,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.BOOL
+        },
+        {
+            key: 'pageFormat',
+            value: 2,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'unitEdgeLength',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 20,
+            isOriginParameter: true
+        },
+        {
+            key: 'newInitialPlacement',
+            value: false,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.BOOL
+        },
+        {
+            key: 'qualityVersusSpeed',
+            value: 2,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'randSeed',
+            type: PARAMETER_TYPE.INT,
+            value: 100,
+            isOriginParameter: true
+        },
+        {
+            key: 'edgeLengthMeasurement',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'allowedPositions',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'maxIntPosExponent',
+            type: PARAMETER_TYPE.INT,
+            value: 40,
+            isOriginParameter: true
+        },
+        {
+            key: 'pageRatio',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 1,
+            isOriginParameter: true
+        },
+        {
+            key: 'stepsForRotatingComponents',
+            type: PARAMETER_TYPE.INT,
+            value: 10,
+            isOriginParameter: true
+        },
+        {
+            key: 'tipOverCCs',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'minDistCC',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 100,
+            isOriginParameter: true
+        },
+        {
+            key: 'presortCCs',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'minGraphSize',
+            type: PARAMETER_TYPE.INT,
+            value: 50,
+            isOriginParameter: true
+        },
+        {
+            key: 'galaxyChoice',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'randomTries',
+            type: PARAMETER_TYPE.INT,
+            value: 20,
+            isOriginParameter: true
+        },
+        {
+            key: 'maxIterChange',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'maxIterFactor',
+            type: PARAMETER_TYPE.INT,
+            value: 10,
+            isOriginParameter: true
+        },
+        {
+            key: 'initialPlacementMult',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'forceModel',
+            value: 2,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'springStrength',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 1,
+            isOriginParameter: true
+        },
+        {
+            key: 'repForcesStrength',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 1,
+            isOriginParameter: true
+        },
+        {
+            key: 'repulsiveForcesCalculation',
+            value: 2,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'stopCriterion',
+            value: 2,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'threshold',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 0.01,
+            isOriginParameter: true
+        },
+        {
+            key: 'fixedIterations',
+            value: 30,
+            type: PARAMETER_TYPE.INT,
+            isOriginParameter: true
+        },
+        {
+            key: 'forceScalingFactor',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 0.05,
+            isOriginParameter: true
+        },
+        {
+            key: 'coolTemperature',
+            value: false,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.BOOL
+        },
+        {
+            key: 'coolValue',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 0.99,
+            isOriginParameter: true
+        },
+        {
+            key: 'initialPlacementForces',
+            value: 2,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'resizeDrawing',
+            value: true,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.BOOL
+        },
+        {
+            key: 'resizingScalar',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 1,
+            isOriginParameter: true
+        },
+        {
+            type: PARAMETER_TYPE.INT,
+            key: 'fineTuningIterations',
+            value: 20,
+            isOriginParameter: true
+        },
+        {
+            key: 'fineTuneScalar',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 0.2,
+            isOriginParameter: true
+        },
+        {
+            key: 'adjustPostRepStrengthDynamically',
+            value: true,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.BOOL
+        },
+        {
+            key: 'postSpringStrength',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 2,
+            isOriginParameter: true
+        },
+        {
+            key: 'postStrengthOfRepForces',
+            type: PARAMETER_TYPE.DOUBLE,
+            value: 0.01,
+            isOriginParameter: true
+        },
+        {
+            type: PARAMETER_TYPE.INT,
+            key: 'frGridQuotient',
+            value: 2,
+            isOriginParameter: true
+        },
+        {
+            key: 'nmTreeConstruction',
+            value: 1,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            key: 'nmSmallCell',
+            value: 0,
+            isOriginParameter: true,
+            type: PARAMETER_TYPE.CATEGORICAL
+        },
+        {
+            type: PARAMETER_TYPE.INT,
+            key: 'nmParticlesInLeaves',
+            value: 25,
+            isOriginParameter: true
+        },
+        {
+            type: PARAMETER_TYPE.INT,
+            key: 'nmPrecision',
+            value: 4,
+            isOriginParameter: true
+        },
+        {
+            key: 'useWorker',
+            value: false,
+            isOriginParameter: false,
+            type: PARAMETER_TYPE.BOOL
+        }
+        /* #endregion */
+    ]
+
     test("Get fm3's default parameter entries", () => {
         const entries = getParameterEntries(
             fm3DefaultParameters,
-            ogdf.layouts.energybased.fm3.ORIGIN_PARAMETER_DEFINITION,
-            ogdf.layouts.energybased.fm3.OUTER_PARAMETER_DEFINITION
+            fm3Class.ORIGIN_PARAMETER_DEFINITION,
+            fm3Class.OUTER_PARAMETER_DEFINITION
         )
-        const expected = [
-            {
-                key: 'useHighLevelOptions',
-                value: false,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.BOOL
-            },
-            {
-                key: 'singleLevel',
-                value: false,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.BOOL
-            },
-            {
-                key: 'pageFormat',
-                value: 2,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'unitEdgeLength',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 20,
-                isOriginParameter: true
-            },
-            {
-                key: 'newInitialPlacement',
-                value: false,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.BOOL
-            },
-            {
-                key: 'qualityVersusSpeed',
-                value: 2,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'randSeed',
-                type: PARAMETER_TYPE.INT,
-                value: 100,
-                isOriginParameter: true
-            },
-            {
-                key: 'edgeLengthMeasurement',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'allowedPositions',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'maxIntPosExponent',
-                type: PARAMETER_TYPE.INT,
-                value: 40,
-                isOriginParameter: true
-            },
-            {
-                key: 'pageRatio',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 1,
-                isOriginParameter: true
-            },
-            {
-                key: 'stepsForRotatingComponents',
-                type: PARAMETER_TYPE.INT,
-                value: 10,
-                isOriginParameter: true
-            },
-            {
-                key: 'tipOverCCs',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'minDistCC',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 100,
-                isOriginParameter: true
-            },
-            {
-                key: 'presortCCs',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'minGraphSize',
-                type: PARAMETER_TYPE.INT,
-                value: 50,
-                isOriginParameter: true
-            },
-            {
-                key: 'galaxyChoice',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'randomTries',
-                type: PARAMETER_TYPE.INT,
-                value: 20,
-                isOriginParameter: true
-            },
-            {
-                key: 'maxIterChange',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'maxIterFactor',
-                type: PARAMETER_TYPE.INT,
-                value: 10,
-                isOriginParameter: true
-            },
-            {
-                key: 'initialPlacementMult',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'forceModel',
-                value: 2,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'springStrength',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 1,
-                isOriginParameter: true
-            },
-            {
-                key: 'repForcesStrength',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 1,
-                isOriginParameter: true
-            },
-            {
-                key: 'repulsiveForcesCalculation',
-                value: 2,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'stopCriterion',
-                value: 2,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'threshold',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 0.01,
-                isOriginParameter: true
-            },
-            {
-                key: 'fixedIterations',
-                value: 30,
-                type: PARAMETER_TYPE.INT,
-                isOriginParameter: true
-            },
-            {
-                key: 'forceScalingFactor',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 0.05,
-                isOriginParameter: true
-            },
-            {
-                key: 'coolTemperature',
-                value: false,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.BOOL
-            },
-            {
-                key: 'coolValue',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 0.99,
-                isOriginParameter: true
-            },
-            {
-                key: 'initialPlacementForces',
-                value: 2,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'resizeDrawing',
-                value: true,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.BOOL
-            },
-            {
-                key: 'resizingScalar',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 1,
-                isOriginParameter: true
-            },
-            {
-                type: PARAMETER_TYPE.INT,
-                key: 'fineTuningIterations',
-                value: 20,
-                isOriginParameter: true
-            },
-            {
-                key: 'fineTuneScalar',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 0.2,
-                isOriginParameter: true
-            },
-            {
-                key: 'adjustPostRepStrengthDynamically',
-                value: true,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.BOOL
-            },
-            {
-                key: 'postSpringStrength',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 2,
-                isOriginParameter: true
-            },
-            {
-                key: 'postStrengthOfRepForces',
-                type: PARAMETER_TYPE.DOUBLE,
-                value: 0.01,
-                isOriginParameter: true
-            },
-            {
-                type: PARAMETER_TYPE.INT,
-                key: 'frGridQuotient',
-                value: 2,
-                isOriginParameter: true
-            },
-            {
-                key: 'nmTreeConstruction',
-                value: 1,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                key: 'nmSmallCell',
-                value: 0,
-                isOriginParameter: true,
-                type: PARAMETER_TYPE.CATEGORICAL
-            },
-            {
-                type: PARAMETER_TYPE.INT,
-                key: 'nmParticlesInLeaves',
-                value: 25,
-                isOriginParameter: true
-            },
-            {
-                type: PARAMETER_TYPE.INT,
-                key: 'nmPrecision',
-                value: 4,
-                isOriginParameter: true
-            },
-            {
-                key: 'useWorker',
-                value: false,
-                isOriginParameter: false,
-                type: PARAMETER_TYPE.BOOL
-            }
-        ]
 
-        expect(entries).toEqual(expected)
+        expect(entries).toEqual(defaultExpected)
     })
-    // updateParameters(fm3DefaultParameters, {}, ogdf.fm3.PARAMETER_DEFINITION)
+
+    test("Get fm3's parameter entries after updateParameters(empty)", () => {
+        updateParameters(fm3DefaultParameters, {}, fm3Class.PARAMETER_DEFINITION)
+
+        const entries = getParameterEntries(
+            fm3DefaultParameters,
+            fm3Class.ORIGIN_PARAMETER_DEFINITION,
+            fm3Class.OUTER_PARAMETER_DEFINITION
+        )
+
+        expect(entries).toEqual(defaultExpected)
+    })
+
+    test("Get fm3's parameter entries after updateParameters", () => {
+        const newParameter = {
+            singleLevel: true,
+            pageFormat: 'Portrait',
+            unitEdgeLength: 10,
+            fixedIterations: 1
+        }
+
+        updateParameters(fm3DefaultParameters, newParameter, fm3Class.PARAMETER_DEFINITION)
+
+        const entries = getParameterEntries(
+            fm3DefaultParameters,
+            fm3Class.ORIGIN_PARAMETER_DEFINITION,
+            fm3Class.OUTER_PARAMETER_DEFINITION
+        )
+
+        const newParameterValue = {
+            singleLevel: true,
+            pageFormat: 0,
+            unitEdgeLength: 10,
+            fixedIterations: 1
+        }
+        defaultExpected.forEach((entry) => {
+            if (entry.key in newParameterValue) {
+                entry.value = newParameterValue[entry.key]
+            }
+        })
+
+        expect(entries).toEqual(defaultExpected)
+    })
+
+    // TODO test layouts with embedded modules
 })
